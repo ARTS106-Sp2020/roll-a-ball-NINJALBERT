6c67a7b2c4636e7b8694491e8e2220875eed6b95
NINJALBERT <AlbertRagerIV@users.noreply.github.com>
2020-05-07 17:12:18 -0400

    Title Screen Complete

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Animator startButton;
+    public Animator infoDialog;
+
+    public void StartGame() 
+    {
+    	SceneManager.LoadScene("Roll-a-ball");
+    }
+
+    public void Quit()
+    {
+	Application.Quit();
+    }
+
+    public void OpenInfo()
+    {
+	startButton.SetBool("isHidden", false);
+	infoDialog.SetBool("isHidden", false);
+    }
+
+    public void CloseInfo()
+    {
+	startButton.SetBool("isHidden", true);
+	infoDialog.SetBool("isHidden", true);
+    }
+
+}
